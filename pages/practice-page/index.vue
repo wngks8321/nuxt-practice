20cab3b03701443710d5868bbfbe6308b24f7d2d
wngks8321 <wngks8321@gmail.com>
2022-04-17 15:22:10 +0900

    버튼 컴포넌트 수정

--- pages/practice-page/index.vue
+++ pages/practice-page/index.vue
@@ -1,12 +1,12 @@
 <template>
   <div>
-    <button type='button' class='btn_small'>
+    <button type='button' class='btn_common btn_small'>
       버튼1
     </button>
-    <button type='button' class='btn_medium'>
+    <button type='button' class='btn_common btn_medium'>
       버튼2
     </button>
-    <button type='button' class='btn_large'>
+    <button type='button' class='btn_common btn_large'>
       버튼3
     </button>
   </div>
